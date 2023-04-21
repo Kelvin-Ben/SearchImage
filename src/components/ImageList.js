833a56c9db8f797c02d346c01adab6206f0051dc
Kelvin-Ben <kevoben14@gmail.com>
2023-04-21 10:00:18 +0300

    Rendered a list of photos from the unsplash api

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 const ImageList = (props) => {
-  console.log(props.images);
+  // console.log(props.images);
+  const images = props.images.map(({id, description, urls}) => {
+    return <img key={id} alt = {description} src= {urls.regular} />
+  })
   return (
-    <div>ImageList</div>
+    <div>
+      {images}
+    </div>
   )
 }
 
 export default ImageList
+
